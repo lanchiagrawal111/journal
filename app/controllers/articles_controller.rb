66e773fddfce4987d6945e27f5c70d654cf656c1
lanchi <lanchiagrawal11@gmail.com>
2021-11-19 19:48:33 +0530

    journal app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "lanchi", password: "lanchi", except: [:index, :show]
+
+  def index
+    @articles  = Article.paginate(:page => params[:page], :per_page=>2)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end  
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+
+end
